136c2d01f7c017d42d4c83e7e227172f4499a523
snjvgitload <sanjeevaakash.snjv@gmail.com>
2024-11-17 19:47:51 +0530

    enabled auto configuaration

--- src/main/java/com/example/demo/DsaApplication.java
+++ src/main/java/com/example/demo/DsaApplication.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 
 
 
 @SpringBootApplication
+@EnableAutoConfiguration()
+@EnableMongoRepositories
 public class DsaApplication {
 
 	public static void main(String[] args) {
