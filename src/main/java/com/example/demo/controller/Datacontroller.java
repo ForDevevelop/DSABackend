28063c9bc58e844d9964899e3d7520b8cac57b89
snjvgitload <sanjeevaakash.snjv@gmail.com>
2024-11-17 23:05:33 +0530

    hi

--- src/main/java/com/example/demo/controller/Datacontroller.java
+++ src/main/java/com/example/demo/controller/Datacontroller.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.demo.repository.MongoRepo;
 import com.example.demo.service.MongoService;
 
 import io.swagger.v3.oas.annotations.Operation;
